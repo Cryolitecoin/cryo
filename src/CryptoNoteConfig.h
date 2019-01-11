e4ecba4f4d217f65217bc15371df4090b49c42ad
Cryolitecoin <38926162+Cryolitecoin@users.noreply.github.com>
2019-01-11 06:14:17 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -113,13 +113,13 @@ const char     MINER_CONFIG_FILE_NAME[]                      	= "miner_conf.json
 
 } // parameters
 
-const uint64_t START_BLOCK_REWARD				= (UINT64_C(5000) * parameters::POINT); // start reward
-const uint64_t FOUNDATION_TRUST					= (UINT64_C(12000000) * parameters::COIN); // locked funds to secure network
-const uint64_t MAX_BLOCK_REWARD					= (UINT64_C(20) * parameters::COIN); // max reward
-const uint64_t REWARD_INCREASE_INTERVAL				= (UINT64_C(21900)); // aprox. 1 month (+ 0.25 increment per month)
+const uint64_t START_BLOCK_REWARD				= (UINT64_C(50000) * parameters::POINT); // start reward
+const uint64_t FOUNDATION_TRUST					= (UINT64_C(120000) * parameters::COIN); // locked funds to secure network
+const uint64_t MAX_BLOCK_REWARD					= (UINT64_C(10) * parameters::COIN); // max reward
+const uint64_t REWARD_INCREASE_INTERVAL				= (UINT64_C(22900)); // aprox. 1 month (+ 0.25 increment per month)
 
 const char     CRYPTONOTE_NAME[]                             	= "cryofast";
-const char     GENESIS_COINBASE_TX_HEX[]			= "010a01ff0001c096b102029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101a19133649262d2f8e1b79c44527bd4c63ec26e9d7a959977f21c9a421bc3e03d";
+const char     GENESIS_COINBASE_TX_HEX[]			= "";
 const uint32_t GENESIS_NONCE                         	        = 10000;
 const uint64_t GENESIS_TIMESTAMP				= 1527078920;
 
